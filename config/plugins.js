6807c75703a3ddbc4e1660491f13dd4e6d29721c
Pankaj Surya <pankajsurya1998@outlook.com>
2026-08-05 11:24:15 +0530

    SL changes for image upload issue

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,26 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: env.int('UPLOAD_SIZE_LIMIT', 50 * 1024 * 1024),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+      security: {
+        file: {
+          maxSize: env.int('UPLOAD_MAX_FILE_SIZE', 50 * 1024 * 1024),
+          allowedTypes: [
+            'image/jpeg',
+            'image/png',
+            'image/gif',
+            'image/webp',
+            'image/svg+xml',
+          ],
+        },
+      },
+    },
+  },
+});
